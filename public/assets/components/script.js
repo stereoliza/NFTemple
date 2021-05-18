5ba7ca2f0c1c44c055c36c10535aedfcc12a62cb
stereotartarus <80116954+stereotartarus@users.noreply.github.com>
2021-05-18 03:17:04 +0300

    Add files via upload

---
+++ public/assets/components/script.js
@@ -0,0 +1,42 @@
+function saveLink() {
+    let links = document.getElementsByClassName("link");
+    let i =   links.length - 1;
+
+    while (i >= 0){
+        let checkbox = links.item(i).getElementsByTagName("input").item(0).checked;
+        if (checkbox == true){
+            let link = links.item(i).getElementsByTagName("input").item(1).value;
+            let k = links.item(i).getElementsByTagName("input").item(0).name;
+            sessionStorage.setItem(k, link);
+
+        }
+        i = i - 1;
+    }
+
+}
+let count = 1;
+function addItem() {
+
+    let check = document.createElement("input");
+    check.type = "checkbox";
+    check.name = "item"+count;
+    check.value = "0";
+
+    var textnode = document.createTextNode("IPFS Item:");
+
+    let input = document.createElement("input");
+    input.type = "text";
+    input.value = "val";
+
+    let form = document.createElement("form");
+    form.onclick = "saveLink()";
+    form.className = "link";
+
+    form.appendChild(check);
+    form.appendChild(textnode);
+    form.appendChild(input);
+
+    document.getElementById("list").appendChild(form);
+
+    count += 1;
+}
